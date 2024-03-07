7a083cc4ba39351272ddfcaa9a667832bb57d410
prince-raj-17 <princeraj991217@gmail.com>
2024-03-07 13:16:22 +0530

    added revising-the-select-query-2 and renamed subfolder

---
+++ Basic_Select/revising-the-select-query-2.sql
@@ -0,0 +1,22 @@
+/*
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. 
+The CountryCode for America is USA.
+
+The CITY table is described as follows:
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
+
+-- Solution:
+SELECT name 
+FROM city
+WHERE population > 120000
+AND countrycode = 'USA';

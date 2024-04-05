8a96c8a3be58c89f0850275455d76573972dc4ee
prince-raj-17 <princeraj991217@gmail.com>
2024-04-05 12:18:09 +0530

    updated

--- Basic_Select/weather-observation-station-1.sql
+++ Basic_Select/weather-observation-station-1.sql
@@ -1,5 +1,8 @@
 /*
-Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
+Query a list of CITY and STATE from the STATION table.
+
+The STATION table is described as follows:
+
 +-------------+------------+
 | Field       |   Type     |
 +-------------+------------+
@@ -9,10 +12,10 @@ Given a table STATION that holds data for five fields namely ID, CITY, STATE, NO
 | LAT_N       | NUMERIC    |
 | LONG_W      | NUMERIC    |
 +-------------+------------+
- 
-Write a query to print the list of CITY and STATE in lexicographical order of city and state, 
-i.e., if there are two or more cities with same name arrange these by lexicographical order of state.
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
 */
+
 
 -- Solution:
 
